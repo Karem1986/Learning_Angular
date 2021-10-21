792af4380e0d7284e1a65a5fde811e808791eb92
Karem1986 <karem.ortiz@outlook.com>
2021-10-21 18:31:36 +0200

    still working on routing navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full', //user can go back to home page
+  },
+  {
     path: 'home',
     loadChildren: () =>
       import('./products/products.module').then((m) => m.ProductsModule),
